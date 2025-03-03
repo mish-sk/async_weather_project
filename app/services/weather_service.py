cc2541c7aa5fb9f9baeee55dcabf3d00a7fc9a61
Mykhailo Sotnikov <sotnikov.mykhail@gmail.com>
2025-03-03 15:55:17 +0100

    Fix weather_service.py

--- app/services/weather_service.py
+++ app/services/weather_service.py
@@ -11,7 +11,7 @@ if not WEATHER_API_KEY:
     raise ValueError("WEATHER_API_KEY is missing.")
 
 
-async def weather_fetch(city: str):
+def weather_fetch(city: str) -> dict:
 
     base_url = "http://api.weatherapi.com/v1/current.json"
 
@@ -21,22 +21,21 @@ async def weather_fetch(city: str):
         "aqi": "no"
     }
 
-    async with httpx.AsyncClient() as client:
-        try:
-            response = await client.get(base_url, params=params)
-            response.raise_for_status()
-            data = response.json()
+    try:
+        response = httpx.get(base_url, params=params)
+        response.raise_for_status()
+        data = response.json()
 
-            return {
-                "city": city,
-                "temperature": data["current"]["temp_c"],
-                "description": data["current"]["condition"]["text"]
-            }
+        return {
+            "city": city,
+            "temperature": data["current"]["temp_c"],
+            "description": data["current"]["condition"]["text"]
+        }
 
-        except httpx.HTTPStatusError as e:
-            print(f"Error: {e}")
-            return {"error": f"API error for {city} {e.response.status_code}"}
+    except httpx.HTTPStatusError as e:
+        print(f"Error: {e}")
+        return {"error": f"API error for {city} {e.response.status_code}"}
 
-        except Exception as e:
-            print(f"Error: {e}")
-            return {"error": f"Unexpected error for {city} {str(e)}"}
+    except Exception as e:
+        print(f"Error: {e}")
+        return {"error": f"Unexpected error for {city} {str(e)}"}
