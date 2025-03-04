947eb4a85eaf31da75d4bc70a2ae0c10a2242db6
Mykhailo Sotnikov <sotnikov.mykhail@gmail.com>
2025-03-04 20:16:27 +0100

    Add get results/region endpoint

--- app/main.py
+++ app/main.py
@@ -1,3 +1,6 @@
+import json
+import os
+
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from typing import List
@@ -7,6 +10,7 @@ from app.services.weather_service import weather_fetch
 from app.tasks import fetch_weather_task
 
 
+RESULTS_DIR = "weather_data"
 app = FastAPI()
 
 
@@ -32,3 +36,19 @@ def get_weather(request: CityRequest):
 def get_task_status(task_id: str):
     task = fetch_weather_task.AsyncResult(task_id)
     return {"task_id": task_id, "status": task.status, "result": task.result}
+
+
+@app.get("/results/{region}")
+def get_results(region: str):
+    region_path = os.path.join(RESULTS_DIR, region.lower())
+
+    if not os.path.exists(region_path):
+        raise HTTPException(status_code=404, detail=f"Region {region} not found")
+
+    results = []
+    for file_name in os.listdir(region_path):
+        file_path = os.path.join(region_path, file_name)
+        with open(file_path, "r", encoding="utf-8") as f:
+            results.append(json.load(f))
+
+    return {"region": region, "data": results}
